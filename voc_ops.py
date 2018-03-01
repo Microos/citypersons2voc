dc08184e1c5c87c44b18fcae9ebe3932a2bdf640
Microos <microos316@gmail.com>
2018-03-01 17:22:09 +0800

    pathlib & string compatiable

--- voc_ops.py
+++ voc_ops.py
@@ -123,7 +123,7 @@ class voc_formatter():
         content += tail
         xml_name = img_name.split('.')[0] + '.xml'
         xml_file = self.anno_dir / xml_name
-        with open(xml_file, 'w') as f:
+        with open(str(xml_file), 'w') as f:
             f.write(content)
 
     def __run(self, src_img_dir: Path, name_bbs_dict: dict, name_set):
@@ -153,7 +153,7 @@ class voc_formatter():
 
     def __write_set_file(self, name_set, tv):
         set_file = self.set_dir / tv
-        with open(set_file, 'w') as f:
+        with open(str(set_file), 'w') as f:
             f.write('\n'.join(name_set))
 
     def run(self):
